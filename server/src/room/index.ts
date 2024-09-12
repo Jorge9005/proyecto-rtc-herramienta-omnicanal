9ca40dd5760a816d5d63df9cbb211a496ea69a07
Jorge Sandoval <sanndovalpe.jorgeluis735@outlook.com>
2024-09-12 17:04:21 -0600

    Proyect base

---
+++ server/src/room/index.ts
@@ -0,0 +1,44 @@
+import { Socket } from "socket.io"
+import { v4 as uuidV4 } from "uuid";
+
+const rooms: Record<string, string[]> = {}
+
+interface IRoomParams {
+    roomId: string;
+    peerId: string;
+
+}
+
+export const roomHandler = (socket: Socket) => {
+    const createRoom = () => {
+        const roomId = uuidV4();
+        rooms[roomId] = [];
+        socket.emit("room-created", { roomId });
+        console.log("user created the room", roomId);
+    }
+
+    const joinRoom = ({roomId, peerId}: IRoomParams) => {
+        if(rooms[roomId]){
+            console.log("user joined the room", roomId, peerId);
+            rooms[roomId].push(peerId);
+            socket.join(roomId);
+            socket.emit("get-users", {
+                roomId,
+                participants: rooms[roomId],
+            });
+        }
+
+        socket.on("disconnect", () => {
+            console.log("user left the room", peerId);
+            leaveRoom({roomId, peerId});
+        });
+    };
+
+    const leaveRoom = ({peerId, roomId}: IRoomParams) => {
+        rooms[roomId] = rooms[roomId].filter((id) => id !== peerId);
+        socket.to(roomId).emit("user-disconnected", peerId);
+    };
+
+    socket.on("create-room", createRoom);
+    socket.on("join-room", joinRoom);
+}
